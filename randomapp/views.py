a32cc8f60eba8627757178ecf8ee9e09294e63bc
amoliavko <ittgml@gmail.com>
2017-06-12 02:44:27 +0300

    new18

--- randomapp/views.py
+++ randomapp/views.py
@@ -14,5 +14,5 @@ def success():
 def index():
     form = InputName()
     if request.method == 'POST':
-        return 'Add name'+str(db.session.query(Table.name))
+        return db.session.query(Table.name)
     return render_template('index.html', form=form)
