a5ff1018256ef31b55c717921dd1686d7ce84ec3
amoliavko <ittgml@gmail.com>
2017-06-11 23:32:36 +0300

    run.py4

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-from my_app import app
+#from my_app import app
 
-app.run()
+#app.run()
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return "Hello"
+
