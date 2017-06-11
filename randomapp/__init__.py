0b3a07b7c27ed87120612e401b35537f6b209248
amoliavko <ittgml@gmail.com>
2017-06-12 01:59:36 +0300

    new2

--- randomapp/__init__.py
+++ randomapp/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
-
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+db = SQLAlchemy(app)
+heroku = Heroku(app)
 
+from randomapp import views
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return 'YES'
+
+
