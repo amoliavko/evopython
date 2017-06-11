e143fb3d485118db8923acfd525980e62901896c
user <ittgml@gmail.com>
2017-06-11 18:51:37 +0300

    artem

--- randomapp/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField
-from wtforms.validators import Required
-
-
-class InputName(Form):
-    name = TextField('name', validators=[Required()])
