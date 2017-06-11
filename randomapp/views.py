a77072669653834dcb43c751d9fab5c92a7a5a03
amoliavko <ittgml@gmail.com>
2017-06-12 02:28:52 +0300

    new13

--- randomapp/views.py
+++ randomapp/views.py
@@ -16,33 +16,6 @@ def index():
     if request.method == 'POST':
         if request.form['submit'] == 'Добавить имя':
             if form.validate_on_submit():
-                if Table.query.filter_by(name=form.name.data).first() is None:
-                    n = Table(name=form.name.data)
-                    db.session.add(n)
-                    db.session.commit()
+
                     return 'Add name'
-        elif request.form['submit'] == 'Удалить имя':
-            if form.validate_on_submit():
-                if Table.query.filter_by(name=form.name.data).first() is not None:
-                    Table.query.filter(Table.name == form.name.data).delete()
-                    db.session.commit()
-                    return 'Delete name ' + form.name.data
-
-        elif request.form['submit'] == 'Выбрать случайных победителей':
-            id_list = []
-            result = []
-            for i in db.session.query(Table.name):
-                id_list.append(i)
-
-            if len(id_list) <= 3:
-                for i in id_list:
-                    result.append(i[-1])
-
-            else:
-                while len(result) < 3:
-                    nm = random.choice(id_list)[-1]
-                    if nm not in result:
-                        result.append(nm)
-            return render_template('/success.html', result=result)
-
     return render_template('index.html', form=form)
