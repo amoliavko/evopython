35f57736784c5ba6f3aedcf865ab794d8c003362
amoliavko <ittgml@gmail.com>
2017-06-12 01:04:30 +0300

    nuuu

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'myk433eyforf346laskJJ80app'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/names_database'
+SQLALCHEMY_DATABASE_URI = 'DATABASE_URL'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost/names_database'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/pre-registration'
 
