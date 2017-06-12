ce1d8a97345b55df19d74ab892268b4af1e91a46
amoliavko <ittgml@gmail.com>
2017-06-12 11:34:35 +0300

    work7

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
-from flask.ext.wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField
 from wtforms.validators import Required
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
 
-class InputName(Form):
+
+class InputName(FlaskForm):
     name = TextField('name', validators=[Required()])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = InputName()
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			return render_template('index.html', form=form)
+    form = InputName()
+    if form.validate_on_submit():
+        return render_template('index.html', form=form)
+    return render_template('index.html', form=form)
 
 
+app.run(debug=True)
