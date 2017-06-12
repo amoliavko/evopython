8fa158b6e3ea53d35a04ccd592110794230c0f37
amoliavko <ittgml@gmail.com>
2017-06-12 03:59:40 +0300

    new48

--- randomapp/views.py
+++ randomapp/views.py
@@ -1,8 +1,9 @@
 from randomapp import app
 from flask import render_template
-
+from randomapp.forms import InputName
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	form = InputName()
+	return render_template('index.html')
 
