8d99aebb902cbac6ff54001e604e79574ae4e31a
amoliavko <ittgml@gmail.com>
2017-06-12 15:47:47 +0300

    work52

--- randomapp/views.py
+++ randomapp/views.py
@@ -9,46 +9,45 @@ import random
 def index():
     form = InputName()
 
-    if form.is_submitted():
-        if request.method == 'POST':
-            if request.form['submit'] == 'Добавить имя':
-                if form.name.data == '':
-                    return render_template("/index.html", form=form, error=form.name.data)
-                elif Table.query.filter_by(name=form.name.data).first() is None:
-                    n = Table(name=form.name.data)
-                    db.session.add(n)
+    if request.method == 'POST':
+        if request.form['submit'] == 'Добавить имя':
+            if form.name.data == '':
+                return render_template("/index.html", form=form, error=form.name.data)
+            elif Table.query.filter_by(name=form.name.data).first() is None:
+                n = Table(name=form.name.data)
+                db.session.add(n)
+                db.session.commit()
+                return render_template("/index.html", form=form, succuss_add=form.name.data)
+            else:
+                return render_template("/index.html", form=form, error_add_already=form.name.data)
+
+        elif request.form['submit'] == 'Удалить имя':
+            if form.name.data == '':
+                return render_template("/index.html", form=form, error=form.name.data)
+            elif Table.query.filter_by(name=form.name.data).first():
+                    Table.query.filter(Table.name == form.name.data).delete()
                     db.session.commit()
-                    return render_template("/index.html", form=form, succuss_add=form.name.data)
-                else:
-                    return render_template("/index.html", form=form, error_add_already=form.name.data)
+                    return render_template("/index.html", form=form, succuss_del=form.name.data)
+            else:
+                return render_template("/index.html", form=form, error_del='error')
 
-            elif request.form['submit'] == 'Удалить имя':
-                if form.name.data == '':
-                    return render_template("/index.html", form=form, error=form.name.data)
-                elif Table.query.filter_by(name=form.name.data).first():
-                        Table.query.filter(Table.name == form.name.data).delete()
-                        db.session.commit()
-                        return render_template("/index.html", form=form, succuss_del=form.name.data)
-                else:
-                    return render_template("/index.html", form=form, error_del='error')
+        elif request.form['submit'] == 'Выбрать случайных победителей':
+            id_list = []
+            result = []
+            for i in db.session.query(Table.name):
+                id_list.append(i)
 
-            elif request.form['submit'] == 'Выбрать случайных победителей':
-                id_list = []
-                result = []
-                for i in db.session.query(Table.name):
-                    id_list.append(i)
-
-                if len(id_list) <= 3:
-                    for i in id_list:
-                        result.append(i[-1])
-                else:
-                    while len(result) < 3:
-                        nm = random.choice(id_list)[-1]
-                        if nm not in result:
-                            result.append(nm)
-                return render_template('/success.html', result=result)
+            if len(id_list) <= 3:
+                for i in id_list:
+                    result.append(i[-1])
+            else:
+                while len(result) < 3:
+                    nm = random.choice(id_list)[-1]
+                    if nm not in result:
+                        result.append(nm)
+            return render_template('/success.html', result=result)
 
         if request.form['submit'] == 'Повторить':
             return render_template('/index.html', form=form)
 
-        return render_template('/index.html', form=form)
+    return render_template('/index.html', form=form)
