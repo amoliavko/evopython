04a8a2e2e2b0fc47cdef1575ea72ac150ba45dc1
amoliavko <ittgml@gmail.com>
2017-06-12 12:08:07 +0300

    work10

--- randomapp/views.py
+++ randomapp/views.py
@@ -1,12 +1,32 @@
-from randomapp import app
-
+from randomapp import app, db
+from randomapp.model import Table
 from flask import render_template, redirect, request
 from randomapp.forms import InputName
-
+import random
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputName()
-    if form.validate_on_submit():
-        return render_template('index.html', form=form)
+    if request.method == 'POST':
+        if request.form['submit'] == 'Добавить имя':
+            if form.name.data:
+                return 'yey '
+
+    elif request.form['submit'] == 'Выбрать случайных победителей':
+        id_list = []
+        result = []
+        for i in db.session.query(Table.name):
+            id_list.append(i)
+
+        if len(id_list) <= 3:
+            for i in id_list:
+                result.append(i[-1])
+
+        else:
+            while len(result) <= 3:
+                nm = random.choice(id_list)[-1]
+                if nm not in result:
+                    result.append(nm)
+        return render_template('/success.html', result=result)
+
     return render_template('index.html', form=form)
