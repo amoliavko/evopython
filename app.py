214c315dd95506a380f63c4a0d0465e14cc72d15
amoliavko <ittgml@gmail.com>
2017-06-12 04:16:49 +0300

    new52

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ class InputName(Form):
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = InputName()
-	return render_template('index.html')
+	return render_template('index.html', form=form)
 
 
