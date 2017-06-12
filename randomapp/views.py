52760ef74c317d3919e69ef5cf41db5a895db230
amoliavko <ittgml@gmail.com>
2017-06-12 13:47:01 +0300

    work30

--- randomapp/views.py
+++ randomapp/views.py
@@ -42,6 +42,6 @@ def index():
                     nm = random.choice(id_list)[-1]
                     if nm not in result:
                         result.append(nm)
-            return render_template('/success.html', result=result)
 
+            return render_template('/index.html', result=result)
     return render_template('/index.html', form=form)
