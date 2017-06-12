261368ef66fbc3f697c4c9eab89fd03bc14cb5fc
amoliavko <ittgml@gmail.com>
2017-06-12 03:19:51 +0300

    new32

--- randomapp/__init__.py
+++ randomapp/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+
 
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return "Hello"
 
 
 
